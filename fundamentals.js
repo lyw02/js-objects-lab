e039b25d3cc5966efa952a1cf437de715d6d635c
Yiwei Liu <2814824250@qq.com>
2023-09-12 18:09:13 +0800

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -2,6 +2,15 @@ const aCar = {
     owner : "Joe Bloggs",
     type : 'Toyota Corolla 1.8',
     registration : '201WD1058'
-  };
-  
-  console.log(aCar.owner);
+};
+
+aCar.mileage = 10000
+aCar.colour = {
+    exteriorColour : "red",
+    interiorFabric : {
+        texture : "leather",
+        shade : "cream"
+    }
+}
+
+console.log(aCar.owner);
