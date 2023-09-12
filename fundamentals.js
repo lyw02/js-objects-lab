c4c828db714138439280bba2472b48c5060cffb4
Yiwei Liu <2814824250@qq.com>
2023-09-12 18:34:25 +0800

    Nested objects.

--- fundamentals.js
+++ fundamentals.js
@@ -5,10 +5,17 @@ const aCar = {
         { owner : "Pat Smith", address : "1 Main Street" }, 
         { owner : "Sheila Dwyer", address : "2 High Street" }
     ],
-    type : 'Toyota Corolla 1.8',
+    type : {
+        make : 'Toyota',
+        model : 'Corolla',
+        cc : 1.8
+    },
     features : ['Parking assist', 'Alarm', 'Tow-bar'],
-    registration : '201WD1058'
+    registration: { year: 201, county: "WD", number: 1058 }
 };
+
+console.log(aCar.owner + ' drives a ' + aCar.type.make)
+console.log("Reg. = " + aCar.registration.year + "-" + aCar.registration.county + "-" + aCar.registration.number);
 
 aCar.mileage = 10000
 aCar.colour = {
